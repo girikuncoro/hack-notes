a282ff1b7de34739d054f0e0ed21f23cdc50d7e2
Giri Kuncoro <girikuncoro@gmail.com>
2018-02-14 12:56:48 -0800

    Change ssh to root opsman

--- pks-push.sh
+++ pks-push.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
 # Pre-requisite: SSH with public/private key
-# From jumphost: $ ssh-keygen -t rsa
-# From jumphost: $ cat ~/.ssh/id_rsa.pub | ssh kubo@<JUMPHOST-IP> 'cat >> .ssh/authorized_keys'
+#     ssh       ssh
+# A ------> B ------> C
+#     ^          ^
+#  using A's   using A's
+#  ssh key     ssh key
+#
+# where A: MacBook, B: Jumphost, C: Opsman VM
+# From Macbookt: 
+# $ ssh-keygen -t rsa
+# $ cat ~/.ssh/id_rsa.pub | ssh kubo@<JUMPHOST-IP> 'cat >> .ssh/authorized_keys'
+# $ scp ~/.ssh/id_rsa.pub kubo@<JUMPHOST-IP>:
+# 
+# From Jumphost:
+# SSH to ubuntu@<REMOTE-SERVER> and enable root SSH access (vi /etc/ssh/sshd_config, edit PermitRootLogin, DenyUsers, and AllowGroups)
+# $ cat ./id_rsa.pub | ssh root@<REMOTE-SERVER> 'cat >> .ssh/authorized_keys'
 
 # Hack for deploying bosh release changes faster to remote testbed
 VERSION=${1}
@@ -11,13 +24,15 @@ JUMPHOST_IP=${3}
 FILE_NAME=$FILE_PREFIX-$VERSION.tgz
 
 JUMPHOST=kubo@$JUMPHOST_IP
-REMOTE_SERVER=ubuntu@30.0.0.5
+REMOTE_SERVER=root@30.0.0.5
 REMOTE_DESTINATION=/var/tempest/releases
 OWNER=tempest-web
 
 bosh create-release --force --version $VERSION --tarball $FILE_NAME
 
-scp -oProxyJump=$JUMPHOST $FILE_NAME $REMOTE_SERVER:
+scp -oProxyJump=$JUMPHOST $FILE_NAME $REMOTE_SERVER:$REMOTE_DESTINATION
 
 FILE_PATH=$REMOTE_DESTINATION/$FILE_NAME
-ssh -t -oProxyJump=$JUMPHOST $REMOTE_SERVER "sudo mv /home/ubuntu/$FILE_NAME $FILE_PATH; sudo chown $OWNER $FILE_PATH; sudo chgrp $OWNER $FILE_PATH"
+ssh -t -oProxyJump=$JUMPHOST $REMOTE_SERVER "sudo chown $OWNER:$OWNER $FILE_PATH;"
+
+echo "$FILE_NAME has been uploaded to remote server"
